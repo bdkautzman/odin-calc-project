5fa9c59e2d51aa8105582b535ea5cffef81c3d29
bdkautzman <briankautzman@gmail.com>
2024-05-06 16:40:04 -0500

    Add calc buttons and display Add event listener functions Add basic initial styling

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Backend functions
 function add(num1, num2){
     return num1 + num2;
 }
@@ -17,7 +18,7 @@ function divide(num1, num2){
     return num1/num2;
 }
 
-let num1 = 0, num2 = 0, op = '';
+let num1 = '', num2 = '', op = '';
 
 function operate(op, num1, num2){
     switch (op) {
@@ -35,5 +36,72 @@ function operate(op, num1, num2){
     
         default:
             return 'Invalid operator';
+    }
+}
+
+
+let disp = ''; // Calculator display
+
+// Frontend functions
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+
+buttons.forEach((button) => {
+    switch (button.textContent) {
+        case '/':
+        case '*':
+        case '-':
+        case '+':
+            button.addEventListener('click', () => {
+                opBtns(button.textContent);
+            });
+            break;
+            
+        case 'C':
+            button.addEventListener('click', () => {
+                clearBtn();
+            });
+            break;
+        
+        case '=':
+            button.addEventListener('click', () => {
+                equalBtn();
+            });
+            break;
+
+        default:
+            button.addEventListener('click', () => {
+                numBtns(button.textContent);
+            });
+            break;
+    }
+
+})
+
+function opBtns(sym){
+    display.textContent = '';
+    op = sym;
+}
+
+function numBtns(sym){
+    if(op === ''){
+        num1 += sym;
+        display.textContent += sym;
+    }else{
+        num2 += sym;
+        display.textContent += sym;
+    }
+}
+
+function clearBtn(){
+    num1 = 0;
+    op = '';
+    num2 = 0;
+    display.textContent = '';
+}
+
+function equalBtn(){
+    if(num1 !== '' && num2 !== '' && op !== ''){
+        display.textContent = operate(op, num1, num2);
     }
 }
