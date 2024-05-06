53cf63dbcabb0e358d1f7e82434cdaec9ea3145d
bdkautzman <briankautzman@gmail.com>
2024-05-06 14:49:01 -0500

    Add operate function

--- scripts.js
+++ scripts.js
@@ -17,20 +17,23 @@ function divide(num1, num2){
     return num1/num2;
 }
 
-console.log(add(1,2));
-console.log(add(-2,500));
-console.log(add(6,-6));
+let num1 = 0, num2 = 0, op = '';
 
-console.log(subtract(-1,-2));
-console.log(subtract(10,-10));
-console.log(subtract(10,1));
+function operate(op, num1, num2){
+    switch (op) {
+        case '+':
+            return add(num1, num2);
+        
+        case '-':
+            return subtract(num1, num2);
 
-console.log(multiply(5,10));
-console.log(multiply(-5,-2));
-console.log(multiply(6, 0));
-console.log(multiply(2.3, 3.6));
+        case '*':
+            return multiply(num1, num2);
 
-console.log(divide(10, 2));
-console.log(divide(1, 0));
-console.log(divide(0, 100));
-console.log(divide(123234, 34));
+        case '/':
+            return divide(num1, num2);
+    
+        default:
+            return 'Invalid operator';
+    }
+}
